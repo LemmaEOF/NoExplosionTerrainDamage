2a35c2378a539ef3d7792576f0bc8bc777ad999e
Meredith Espinosa <goclonefilms@gmail.com>
2022-08-12 14:52:19 -0700

    make the mod

---
+++ src/main/java/gay/lemmaeof/netd/mixin/MixinExplosion.java
@@ -0,0 +1,23 @@
+package gay.lemmaeof.netd.mixin;
+
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Explosion.class)
+public class MixinExplosion {
+	@Shadow
+	@Final
+	@Mutable
+	private Explosion.DestructionType destructionType;
+
+	@Inject(method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)V", at = @At("RETURN"))
+	private void forceNoneDestructionType(CallbackInfo info) {
+		this.destructionType = Explosion.DestructionType.NONE;
+	}
+}
